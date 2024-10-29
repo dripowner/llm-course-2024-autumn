b2d95c5966a234be681209822d38f67f329193c0
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-10-29 22:26:49 +0300

    hw4

--- Homework/04/minhash.py
+++ Homework/04/minhash.py
@@ -21,7 +21,11 @@ class MinHash:
         id документа - нумерация в списке начиная с нуля
         '''
         # TODO:
+        tokens = list(set.union(*corpus_of_texts))
+        mtx = [[token] + [1 if token in text else 0 for text in corpus_of_texts] for token in tokens]
+        df = pd.DataFrame(mtx, columns=["token", *range(len(corpus_of_texts))], index=tokens)
         df.sort_index(inplace=True)
+        df.index = [i for i in range(df.shape[0])]
         return df
     
     def is_prime(self, a):
@@ -53,22 +57,29 @@ class MinHash:
         Вовзращает расстояние Жаккарда для двух сетов. 
         '''
         # TODO:
-        return 
+        return len(set.intersection(set_a, set_b)) / len(set.union(set_a, set_b))
     
     def get_similar_pairs(self, min_hash_matrix) -> list[tuple]:
         '''
         Находит похожих кандидатов. Отдает список из таплов индексов похожих документов, похожесть которых > threshold.
         '''
         # TODO:
-        return 
+        sim_mtx = self.get_similar_matrix(min_hash_matrix)
+        return [(i, j) for i, row in enumerate(sim_mtx) for j, val in enumerate(row) if val > self.threshold and i < j]
     
     def get_similar_matrix(self, min_hash_matrix) -> list[tuple]:
         '''
         Находит похожих кандидатов. Отдает матрицу расстояний
         '''
         # TODO: 
+        minshash_sets = [set(col) for col in min_hash_matrix.T]
+        sim_mtx = [[0] * min_hash_matrix.shape[1] for _ in range(min_hash_matrix.shape[1])]
+        for set_a_id in range(len(minshash_sets)):
+            for set_b_id in range(set_a_id, len(minshash_sets), 1):
+                sim_mtx[set_a_id][set_b_id] = self.get_jaccard_similarity(minshash_sets[set_a_id], minshash_sets[set_b_id])
+                sim_mtx[set_b_id][set_a_id] = sim_mtx[set_a_id][set_b_id]
                 
-        return 
+        return sim_mtx
      
     
     def get_minhash(self, occurrence_matrix: pd.DataFrame) -> np.array:
@@ -76,16 +87,32 @@ class MinHash:
         Считает и возвращает матрицу мин хешей.
 
         '''
-        # TODO:
-        return 
+        # compute prime_num_rows
+        prime_num_rows = occurrence_matrix.shape[0]
+        while True:
+            if self.is_prime(prime_num_rows):
+                break
+            prime_num_rows += 1
+        minhash = np.zeros((self.num_permutations, occurrence_matrix.shape[1] - 1))
+        for perm_id in range(self.num_permutations):
+            perm_occurrence_matrix = occurrence_matrix.copy().iloc[:, 1:]
+            perm = [self.get_new_index(x, perm_id, prime_num_rows) for x in list(occurrence_matrix.index)]
+            perm_occurrence_matrix.index = perm
+            perm_occurrence_matrix = perm_occurrence_matrix.sort_index()
+            min_hash_row = perm_occurrence_matrix.idxmax(axis=0).to_numpy()
+            minhash[perm_id] = min_hash_row
+        # print(minhash)
+        return minhash
 
     
     def run_minhash(self,  corpus_of_texts: list[str]):
-        occurrence_matrix = self.get_occurrence_matrix(corpus_of_texts)
+        texts = [self.tokenize(text) for text in corpus_of_texts]
+        occurrence_matrix = self.get_occurrence_matrix(texts)
+        # print(occurrence_matrix)
         minhash = self.get_minhash(occurrence_matrix)
         similar_pairs = self.get_similar_pairs(minhash)
         similar_matrix = self.get_similar_matrix(minhash)
-        print(similar_matrix)
+        # print(similar_matrix)
         return similar_pairs
     
     
--- Homework/04/minhashlsh.py
+++ Homework/04/minhashlsh.py
@@ -15,7 +15,8 @@ class MinHashLSH(MinHash):
         Возвращает массив из бакетов, где каждый бакет представляет собой N строк матрицы сигнатур.
         '''
         # TODO:
-        return 
+        buckets = np.array_split(minhash, self.num_buckets, axis=0)
+        return buckets
     
     def get_similar_candidates(self, buckets) -> list[tuple]:
         '''
@@ -24,13 +25,22 @@ class MinHashLSH(MinHash):
         Возвращает список из таплов индексов похожих документов.
         '''
         # TODO:
+        similar_candidates = []
+        for bucket in buckets:
+            for i in range(bucket.shape[1]):
+                for j in range(i+1, bucket.shape[1]):
+                    if np.array_equal(bucket[:, i], bucket[:, j]):
+                        similar_candidates.append((i, j))
         return similar_candidates
         
     def run_minhash_lsh(self, corpus_of_texts: list[str]) -> list[tuple]:
-        occurrence_matrix = self.get_occurrence_matrix(corpus_of_texts)
+        texts = [self.tokenize(text) for text in corpus_of_texts]
+        occurrence_matrix = self.get_occurrence_matrix(texts)
         minhash = self.get_minhash(occurrence_matrix)
+        print(minhash)
         buckets = self.get_buckets(minhash)
+        print(buckets[:10])
         similar_candidates = self.get_similar_candidates(buckets)
-        
+
         return set(similar_candidates)
     
