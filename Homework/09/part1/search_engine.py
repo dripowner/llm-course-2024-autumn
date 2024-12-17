6cd3296dec50eb956e0e2e16d775946ea3ffbf4e
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-12-17 19:56:24 +0300

    hw 9

--- Homework/09/part1/search_engine.py
+++ Homework/09/part1/search_engine.py
@@ -5,79 +5,104 @@ import json
 import pickle
 from sentence_transformers import SentenceTransformer
 
+
 @dataclass
 class Document:
-   id: str
-   title: str
-   text: str
-   embedding: Optional[np.ndarray] = None
+    id: str
+    title: str
+    text: str
+    embedding: Optional[np.ndarray] = None
+
 
 @dataclass
 class SearchResult:
-   doc_id: str
-   score: float
-   title: str
-   text: str
+    doc_id: str
+    score: float
+    title: str
+    text: str
+
 
 def load_documents(path: str) -> List[Document]:
-   """Загрузка документов из json файла"""
-   with open(path, 'r', encoding='utf-8') as f:
-       data = json.load(f)
-   return [
-       Document(
-           id=article['id'],
-           title=article['title'],
-           text=article['text'],
-           embedding=None
-       )
-       for article in data['articles']
-   ]
+    """Загрузка документов из json файла"""
+    with open(path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return [
+        Document(
+            id=article["id"],
+            title=article["title"],
+            text=article["text"],
+            embedding=None,
+        )
+        for article in data["articles"]
+    ]
+
 
 class Indexer:
-   def __init__(self, model_name: str = 'all-MiniLM-L6-v2'):
-       self.model = SentenceTransformer(model_name)
-       self.documents: List[Document] = []
-       self.embeddings: Optional[np.ndarray] = None
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
+        self.model = SentenceTransformer(model_name)
+        self.documents: List[Document] = []
+        self.embeddings: Optional[np.ndarray] = None
 
-   def add_documents(self, documents: List[Document]) -> None:
-       """
-       TODO: Реализовать индексацию документов
-       1. Сохранить документы в self.documents
-       2. Получить эмбеддинги для документов используя self.model.encode()
-          Подсказка: для каждого документа нужно объединить title и text
-       3. Сохранить эмбеддинги в self.embeddings
-       """
-       pass
+    def add_documents(self, documents: List[Document]) -> None:
+        """
+        TODO: Реализовать индексацию документов
+        1. Сохранить документы в self.documents
+        2. Получить эмбеддинги для документов используя self.model.encode()
+           Подсказка: для каждого документа нужно объединить title и text
+        3. Сохранить эмбеддинги в self.embeddings
+        """
+        self.documents = documents
+        self.embeddings = self.model.encode(
+            [doc.title + " " + doc.text for doc in documents]
+        )
 
-   def save(self, path: str) -> None:
-       """
-       TODO: Реализовать сохранение индекса
-       1. Сохранить self.documents и self.embeddings в pickle файл
-       """
-       pass
+    def save(self, path: str) -> None:
+        """
+        TODO: Реализовать сохранение индекса
+        1. Сохранить self.documents и self.embeddings в pickle файл
+        """
+        with open(path, "wb") as f:
+            pickle.dump((self.documents, self.embeddings), f)
 
-   def load(self, path: str) -> None:
-       """
-       TODO: Реализовать загрузку индекса
-       1. Загрузить self.documents и self.embeddings из pickle файла
-       """
-       pass
+    def load(self, path: str) -> None:
+        """
+        TODO: Реализовать загрузку индекса
+        1. Загрузить self.documents и self.embeddings из pickle файла
+        """
+        with open(path, "rb") as f:
+            self.documents, self.embeddings = pickle.load(f)
+
 
 class Searcher:
-   def __init__(self, index_path: str, model_name: str = 'all-MiniLM-L6-v2'):
-       """
-       TODO: Реализовать инициализацию поиска
-       1. Загрузить индекс из index_path
-       2. Инициализировать sentence-transformers
-       """
-       self.model = SentenceTransformer(model_name)
-       pass
+    def __init__(self, index_path: str, model_name: str = "all-MiniLM-L6-v2"):
+        """
+        TODO: Реализовать инициализацию поиска
+        1. Загрузить индекс из index_path
+        2. Инициализировать sentence-transformers
+        """
+        self.model = SentenceTransformer(model_name)
+        with open(index_path, "rb") as f:
+            self.documents, self.embeddings = pickle.load(f)
 
-   def search(self, query: str, top_k: int = 5) -> List[SearchResult]:
-       """
-       TODO: Реализовать поиск документов
-       1. Получить эмбеддинг запроса через self.model.encode()
-       2. Вычислить косинусное сходство между запросом и документами
-       3. Вернуть top_k наиболее похожих документов
-       """
-       pass
+    def search(self, query: str, top_k: int = 5) -> List[SearchResult]:
+        """
+        TODO: Реализовать поиск документов
+        1. Получить эмбеддинг запроса через self.model.encode()
+        2. Вычислить косинусное сходство между запросом и документами
+        3. Вернуть top_k наиболее похожих документов
+        """
+        query_embedding = self.model.encode(query)
+        cosine_similarities = np.dot(self.embeddings, query_embedding)
+        top_k_indices = np.argsort(cosine_similarities)[::-1][:top_k]
+        search_results = []
+
+        for i in top_k_indices:
+            search_results.append(
+                SearchResult(
+                    doc_id=self.documents[i].id,
+                    score=float(cosine_similarities[i]),
+                    title=self.documents[i].title,
+                    text=self.documents[i].text,
+                )
+            )
+        return search_results
--- Homework/09/part2/faiss_search.py
+++ Homework/09/part2/faiss_search.py
@@ -5,8 +5,9 @@ import pickle
 from sentence_transformers import SentenceTransformer
 from part1.search_engine import Document, SearchResult
 
+
 class FAISSSearcher:
-    def __init__(self, model_name: str = 'all-MiniLM-L6-v2'):
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
         """
         Инициализация индекса
         """
@@ -18,7 +19,7 @@ class FAISSSearcher:
     def build_index(self, documents: List[Document]) -> None:
         """
         TODO: Реализовать создание FAISS индекса
-        
+
         1. Сохранить документы
         2. Получить эмбеддинги через model.encode()
         3. Нормализовать векторы (faiss.normalize_L2)
@@ -28,46 +29,88 @@ class FAISSSearcher:
             - Обучить индекс (train)
             - Добавить векторы (add)
         """
-        pass
+        self.documents = documents
+        self.embeddings = self.model.encode(
+            [doc.title + " " + doc.text for doc in documents]
+        )
+        faiss.normalize_L2(self.embeddings)
+        self.index = faiss.IndexIVFFlat(
+            faiss.IndexFlatIP(self.dimension), self.dimension, 6
+        )
+        self.index.train(self.embeddings)
+        self.index.add(self.embeddings)
 
     def save(self, path: str) -> None:
         """
         TODO: Реализовать сохранение индекса
-        
+
         1. Сохранить в pickle:
             - documents
             - индекс (faiss.serialize_index)
         """
-        pass
+        with open(path, "wb") as f:
+            pickle.dump((self.documents, faiss.serialize_index(self.index)), f)
 
     def load(self, path: str) -> None:
         """
         TODO: Реализовать загрузку индекса
-        
+
         1. Загрузить из pickle:
             - documents
             - индекс (faiss.deserialize_index)
         """
-        pass
+        with open(path, "rb") as f:
+            self.documents, self.index = pickle.load(f)
+            self.index = faiss.deserialize_index(self.index)
 
     def search(self, query: str, top_k: int = 5) -> List[SearchResult]:
         """
         TODO: Реализовать поиск
-        
+
         1. Получить эмбеддинг запроса
         2. Нормализовать вектор
         3. Искать через index.search()
         4. Вернуть найденные документы
         """
-        pass
+        query_embedding = self.model.encode([query])
+        faiss.normalize_L2(query_embedding)
+        distances, indices = self.index.search(query_embedding, top_k)
+        print(distances)
+        return [
+            SearchResult(
+                self.documents[i].id,
+                float(
+                    np.clip(distance, 0, 1)
+                ),  # тут не понял почему [0,1] диапазон значений, расстояние же может быть [0,2]
+                self.documents[i].title,
+                self.documents[i].text,
+            )
+            for i, distance in zip(indices[0], distances[0])
+        ]
 
-    def batch_search(self, queries: List[str], top_k: int = 5) -> List[List[SearchResult]]:
+    def batch_search(
+        self, queries: List[str], top_k: int = 5
+    ) -> List[List[SearchResult]]:
         """
         TODO: Реализовать batch-поиск
-        
+
         1. Получить эмбеддинги всех запросов
         2. Нормализовать векторы
         3. Искать через index.search()
         4. Вернуть результаты для каждого запроса
         """
-        pass
+        query_embeddings = self.model.encode(queries)
+        faiss.normalize_L2(query_embeddings)
+        distances, indices = self.index.search(query_embeddings, top_k)
+        return [
+            [
+                SearchResult(
+                    self.documents[i].id,
+                    float(distance),
+                    self.documents[i].title,
+                    self.documents[i].text,
+                )
+                for i, distance in zip(indices[j], distances[j])
+            ]
+            for j in range(len(queries))
+        ]
